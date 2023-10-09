ffbd2311b76cb30220c94342814ab854a0bee4df
Ghassen Saaf <saafghassen@gmail.com>
2023-10-09 22:33:37 +0100

    db configuration (#1)
    * changes
    
    * config port
    
    * mongo config
    
    * changes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser()); // Add cookieParser middleware
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
